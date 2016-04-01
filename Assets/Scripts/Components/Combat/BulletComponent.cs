c784b55a5a5376ab58bc3a99b0bb2fbcac579cf8
ModeToCode <darko.martinovikj@gmail.com>
2016-04-01 13:46:00 +0200

    calcultion of the bullet sprite rotation based on the direction opf bullet

--- Assets/Scripts/Components/Combat/BulletComponent.cs
+++ Assets/Scripts/Components/Combat/BulletComponent.cs
@@ -7,10 +7,9 @@ public class BulletComponent : MonoBehaviour {
     public void Initialize(Bullet bullet) {
         this.Bullet = bullet;
         this.Bullet.Destroyed += Destroy;
-        //TODO calculate the direction of the bullet
         //Calculate the rotation angle in order to rotate the sprite so it represents the direction of the bullet
-        //var angle = Mathf.Atan2(this.Bullet.Direction.y, this.Bullet.Direction.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        var angle = Mathf.Atan2(this.Bullet.Direction.y, this.Bullet.Direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     private void Destroy(Bullet obj) {
@@ -24,9 +23,5 @@ public class BulletComponent : MonoBehaviour {
         }
 
         this.gameObject.transform.position = this.Bullet.CurrentPosition;
-        //TODO rotate the sprite to it is facing the right direction
-        //Vector3 lookPosition = this.Bullet.CurrentPosition;
-        //lookPosition += new Vector3(this.Bullet.Direction.y, 0f, this.Bullet.Direction.x);
-        //this.gameObject.transform.LookAt(new Vector3(this.Bullet.CurrentPosition.x, this.Bullet.CurrentPosition.y, );
     }
 }
